f519cd2380f466e13551e7f737aa4f732ca4b386
Luis Martinez <luismartinez@192.168.1.13>
2022-12-20 09:39:58 -0600

    Initial commit

---
+++ src/main/java/com/example/hotelsapisoap/HotelsApiSoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelsapisoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsApiSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsApiSoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hotelsapisoap/HotelsApiSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelsapisoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsApiSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
